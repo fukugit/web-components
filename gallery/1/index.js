dec18d70c562ed567f05dce8a2907d3da7c4252b
fukugit <fukuma1234@gmail.com>
2024-06-08 18:28:37 +0900

    Add flexbox layout debugger

---
+++ gallery/1/index.js
@@ -0,0 +1,42 @@
+
+let debug_flg = false;
+
+start = ()=> {
+  document.getElementById('body')
+  .style.backgroundColor = '#FFFFFF';
+  document.getElementsByClassName('container')[0]
+    .style.backgroundColor= '#FFFFFF';
+  document.getElementsByClassName('gallery')[0]
+    .style.backgroundColor= '#FFFFFF';
+  document.getElementsByClassName('top')[0]
+    .style.backgroundColor= '#FFFFFF';
+  document.getElementsByClassName('contents')[0]
+    .style.backgroundColor= '#FFFFFF';
+  document.getElementsByClassName('image1')[0]
+    .style.backgroundColor= '#FFFFFF';
+}
+  
+debug = ()=> {
+  if (debug_flg) {
+    document.getElementById('body')
+    .style.backgroundColor = 'skyblue';
+    document.getElementsByClassName('container')[0]
+      .style.backgroundColor= 'orange';
+    document.getElementsByClassName('gallery')[0]
+      .style.backgroundColor= 'greenyellow';
+      document.getElementsByClassName('top')[0]
+      .style.backgroundColor= 'pink';
+    document.getElementsByClassName('contents')[0]
+      .style.backgroundColor= 'orange';
+    document.getElementsByClassName('image1')[0]
+      .style.backgroundColor= 'brown';
+  } else {
+    start();
+  }
+  debug_flg = !debug_flg
+
+}
+
+start();
+let button = document.getElementById('debug');
+button.addEventListener('click', debug);
